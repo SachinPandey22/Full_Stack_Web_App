a01347161d9b461ea42e93fa6fd412e9719fc7a4
Sapnil Basnet <vbb23@txstate.edu>
2025-09-24 00:05:59 -0500

    SCRUM-14 added filtering by muscle group for Exercise API

--- gym-log-assignment/backend/exercises/views.py
+++ gym-log-assignment/backend/exercises/views.py
@@ -1,8 +1,15 @@
 # exercises/views.py
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Exercise
 from .serializers import ExerciseSerializer
 
 class ExerciseViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows exercises to be viewed, created, updated, or deleted.
+    Supports filtering by muscle_group.
+    """
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['muscle_group', 'category']

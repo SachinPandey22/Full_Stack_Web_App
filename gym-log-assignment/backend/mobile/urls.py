f75e970cd252889ad4fb29be819fcc94977d73ad
MisanParajuli <bgg66@txstate.edu>
2025-10-05 13:13:19 -0500

    SCRUM-41 Added steps read endpoint
    Implemented list_steps in mobile/views.py to get /api/mobile/steps

--- gym-log-assignment/backend/mobile/urls.py
+++ gym-log-assignment/backend/mobile/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("mobile/devices/<int:pk>", views.revoke_device),  # DELETE
     path("mobile/link", views.link_device),                # POST
     path("mobile/ingest", views.ingest_data),              # POST
+    path("mobile/steps", views.list_steps),
 ]

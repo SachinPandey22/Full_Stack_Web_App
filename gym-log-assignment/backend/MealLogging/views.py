ba28041a2a209fbb0aafac0e0ca2f4383895cf7b
Surendra Khatri <uaj16@txstate.edu>
2025-11-26 09:48:40 -0800

    SCRUM-84 allow multipart uploads and serve media

--- gym-log-assignment/backend/MealLogging/views.py
+++ gym-log-assignment/backend/MealLogging/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.exceptions import ValidationError
 from django.db.models import Sum
 from datetime import date, datetime, timedelta
@@ -31,6 +32,7 @@ class MealViewSet(viewsets.ModelViewSet):
 
     serializer_class = MealSerializer
     permission_classes = [IsAuthenticated]
+    parser_classes = (MultiPartParser, FormParser)
 
     def get_queryset(self):
         """
--- gym-log-assignment/backend/gymlog/urls.py
+++ gym-log-assignment/backend/gymlog/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse, JsonResponse
+from django.conf import settings
+from django.conf.urls.static import static
 from mobile import views as m
 from support import urls as support_urls
 from support.views import chat_with_ai
@@ -67,3 +69,6 @@ urlpatterns = [
     path("api/", include("users.urls")),
     path("api/", include("mobile.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
